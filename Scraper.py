06fedf9edd55056e8b8a80c38f6ce4f9b3199a9b
Austin Mayerhofer <austinm41.99@gmail.com>
2020-01-01 08:51:09 -0800

    added preprocessing for home/away, as well as csv error checking

--- Scraper.py
+++ Scraper.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import numpy as np
+from sys import exit
+
+NUM_COLUMNS = 15
 
 # reads NFL table from teamrankings.com into pandas dataframe
 def df(tag, link):
@@ -11,8 +15,39 @@ def df(tag, link):
     
     return data
 
-OffPPP_df = df('OffPPP', 'https://www.teamrankings.com/nfl/stat/points-per-game?date=2019-12-29')
-DefPPP_df = df('DefPPP', 'https://www.teamrankings.com/nfl/stat/opponent-points-per-game?date=2019-12-29')
+# Read in CSV
+scores_df = pd.read_csv('nfl-games.csv')
 
-data = OffPPP_df.set_index('Team').join(DefPPP_df.set_index('Team'))
+if len(scores_df.columns) != NUM_COLUMNS:
+    print('Invalid number of columns in CSV, should contain') 
+    print('Week, Day, Date, Year, Time,')
+    print('Winner/tie, Location, Loser/tie, Boxscore, PtsW,')
+    print('PtsL, YdsW, TOW, YdsL, TOL')
+    exit()
+
+# Preprocessing
+scores_df = scores_df.drop(['Boxscore', 'YdsW', 'TOW', 'YdsL', 'TOL'], axis=1) # drop unused information
+
+scores_df.loc[scores_df['Location'] == '@', 'HomeTeam'] = scores_df['Loser/tie']
+scores_df.loc[scores_df['Location'] == '@', 'AwayTeam'] = scores_df['Winner/tie']
+scores_df.loc[scores_df['Location'].isnull(), 'HomeTeam'] = scores_df['Winner/tie']
+scores_df.loc[scores_df['Location'].isnull(), 'AwayTeam'] = scores_df['Loser/tie']
+scores_df.loc[scores_df['Location'] == '@', 'PtsHome'] = scores_df['PtsL']
+scores_df.loc[scores_df['Location'] == '@', 'PtsAway'] = scores_df['PtsW']
+scores_df.loc[scores_df['Location'].isnull(), 'PtsHome'] = scores_df['PtsW']
+scores_df.loc[scores_df['Location'].isnull(), 'PtsAway'] = scores_df['PtsL']
+
+scores_df = scores_df.drop(['Winner/tie', 'Location', 'Loser/tie', 'PtsW', 'PtsL'], axis=1)
+
+
+print(scores_df)
+
+'''
+# Read in teamrankings tables
+OffPPG_df = df('OffPPG', 'https://www.teamrankings.com/nfl/stat/points-per-game?date=2019-12-29')
+DefPPG_df = df('DefPPG', 'https://www.teamrankings.com/nfl/stat/opponent-points-per-game?date=2019-12-29')
+
+data = OffPPG_df.set_index('Team').join(DefPPG_df.set_index('Team'))
+
 print(data)
+'''
